d390f6cba08a084e39582b750bf41d3d21161948
heyzameer <khanzameer5890@gmail.com>
2025-05-11 20:38:02 +0530

    users-logout-route

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
-
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 module.exports.registerUser = async (req, res, next) => {
     const errors = validationResult(req);
@@ -53,4 +53,16 @@ module.exports.loginUser = async (req, res, next) => {
 
 module.exports.getUserProfile = async (req, res, next) => {
     res.status(200).json({ user: req.user });
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+    try {
+        const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+        // await userService.blacklistToken(token);
+        await blacklistTokenModel.create({ token });
+        res.clearCookie('token');
+        return res.status(200).json({ message: 'Logout successful' });
+    } catch (error) {
+        next(error);
+    }
 }
--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,16 +1,21 @@
 const userModel = require('../models/user.model');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 module.exports.authUser = async (req, res, next) => {
   
-        const token = req.cookies.token||req.header('Authorization').replace('Bearer ', '');
+        const token = req.cookies.token||req.header('Authorization')?.replace('Bearer ', '');
 
         // console.log(token);
         if (!token) {
             return res.status(401).send({ error: 'Please authenticate.' });
         }
 
+        const blacklistedToken = await blacklistTokenModel.findOne({ token });
+        if (blacklistedToken) {
+            return res.status(401).send({ error: 'Unautherized' });
+        }
         try {
             // Verify the token
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
