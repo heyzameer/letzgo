497c691b693780091241aeea2dc322581e597445
heyzameer <khanzameer5890@gmail.com>
2025-05-13 00:40:34 +0530

    error-notification-home-page

--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const CaptainHome = () => {
-  return (
-    <div>
-      <h1>Captain Home</h1>
+ return (
+    <div className='relative h-screen'>
+      <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+
+      <div>
+        {/* temp img */}
+        <img src='https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif' alt="" className='w-full h-screen object-cover' />
+      </div>
     </div>
-  )
+      )
 }
 
 export default CaptainHome
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='relative h-screen'>
+      <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+
+      <div>
+        {/* temp img */}
+        <img src='https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif' alt="" className='w-full h-screen object-cover' />
+      </div>
+      <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
+        <div className='h-[30%] p-6 bg-white'>
+          <h4 className='text-2xl font-semibold'>Find a trip</h4>
+          <form >
+            <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
+            <input className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full'
+              type="text"
+              placeholder='Add a pick-up location'
+            />
+            <input className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3'
+              type="text"
+              placeholder='Enter your destination' />
+          </form>
+          <button className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full'>
+            Find Trip
+          </button>
+        </div>
+      </div>
     </div>
-  )
+      )
 }
 
 export default Home
