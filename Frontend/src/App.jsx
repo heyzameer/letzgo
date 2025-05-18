d6795fe92329933b9c30be930d1f16903bc6f7da
heyzameer <khanzameer5890@gmail.com>
2025-05-18 14:41:12 +0530

    added-riding-page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,7 @@ import UserLogout from './pages/UserLogout'
 import UserProtectWrapper from './pages/UserProtectWrapper'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import Riding from './pages/Riding'
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="/" element={<Start/>} />
         <Route path="/signup" element={<UserSignup/>} />
         <Route path="/login" element={<UserLogin/>} />
+        <Route path="/riding" element={<Riding/>} />
         <Route path="/captain-signup" element={<CaptainSignup/>} />
         <Route path="/captain-login" element={<CaptainLogin/>} />
         <Route path="/home" element={<UserProtectWrapper><Home/></UserProtectWrapper>} />
